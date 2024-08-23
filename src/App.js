8e48c36a628123ad75b3d4ee1220bf471a92aa3a
Hitkumar <hit98987@gmail.com>
2024-08-22 20:06:05 -0400

    added authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './AppComponents/Home';
+import About from './AppComponents/About';
+import Contact from './AppComponents/Contact';
+import Navbar from './AppComponents/Navbar';
+import { SignIn, useUser } from '@clerk/clerk-react';
 
 const App = () => {
-  return (
-    <div className='min-h-screen bg-slate-100'>
-      <h1>App</h1>
+  const { isSignedIn, user, isLoaded } = useUser();
+  
+  useEffect(() => {
+    console.log("issignedIn", isSignedIn);
+    console.log("user", user);
+    console.log("isloaded", isLoaded);
+  })
+
+  if (!isLoaded) {
+    return null;
+  }
+
+
+  if (!isSignedIn) {
+    return <div className='flex justify-center items-center w-screen h-screen bg-slate-100'>
+      <SignIn fallbackRedirectUrl='/' forceRedirectUrl='/'/>
     </div>
+  }
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
